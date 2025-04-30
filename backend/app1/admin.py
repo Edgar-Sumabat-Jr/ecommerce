92f08d93164320940da902d8ec5394525a5f9f57
Edgar-Sumabat-Jr <edsumabat2@gmail.com>
2025-04-30 00:11:49 -0700

    added user's details in admin dashboard

--- backend/app1/admin.py
+++ backend/app1/admin.py
@@ -1,4 +1,25 @@
+# admin.py
 from django.contrib import admin
 from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    # Add the fields you want to display on the user list page
+    list_display = ('username', 'email', 'first_name', 'last_name', 'birth_date', 'is_staff', 'is_active', 'date_joined')
+
+    # Add the fields to be displayed on the user detail page
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('birth_date',)}),  # Add 'birth_date' field
+    )
+
+    # Add the fields that appear in the user creation form
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('birth_date',)}),  # Add 'birth_date' to the creation form
+    )
+
+    # Optionally, you can also specify which fields are searchable or filterable
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active', 'date_joined')
+
+# Register the custom user model with the custom admin class
+admin.site.register(CustomUser, CustomUserAdmin)
