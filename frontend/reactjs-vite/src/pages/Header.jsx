8d74c3bb47bb67435a79611cd13fddc150bf5f52
Edgar-Sumabat-Jr <edsumabat2@gmail.com>
2025-05-24 17:41:21 +0800

    added light and dark mode toggle

--- frontend/reactjs-vite/src/pages/Header.jsx
+++ frontend/reactjs-vite/src/pages/Header.jsx
@@ -4,6 +4,7 @@ import { useAuth } from "../context/AuthContext"
 import ThemeToggle from "../components/ThemeToggle";
 
 import '../styles/header.css'
+import '../index.css'
 
 import { Container, Nav, Navbar, NavbarBrand } from 'react-bootstrap';
 
@@ -17,33 +18,33 @@ function Header() {
   }
 
   return (
-  <>
-    <Navbar bg="light" expand="lg" className="custom-navbar">
-  <Container fluid className="px-3"> {/* remove vertical padding here */}
-    <Navbar.Brand as={Link} to="/">MyApp</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link as={Link} to="/" className="custom-nav-link">Home</Nav.Link>
-        {user ? (
-          <Nav.Link onClick={handleLogout} className="custom-nav-link">Logout</Nav.Link>
-        ) : (
-          <>
-            <Nav.Link as={Link} to="/login" className="custom-nav-link">Login</Nav.Link>
-            <Nav.Link as={Link} to="/register" className="custom-nav-link">Register</Nav.Link>
-          </>
-        )}
-      </Nav>
-      <div className="ms-auto">
-        <ThemeToggle />
-      </div>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+    <>
+      <Navbar expand="lg" className="custom-navbar shadow">
+        <Container fluid className="px-3"> {/* remove vertical padding here */}
+          <Navbar.Brand as={Link} to="/">MyApp</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/" className="custom-nav-link">Home</Nav.Link>
+              {user ? (
+                <Nav.Link onClick={handleLogout} className="custom-nav-link">Logout</Nav.Link>
+              ) : (
+                <>
+                  <Nav.Link as={Link} to="/login" className="custom-nav-link">Login</Nav.Link>
+                  <Nav.Link as={Link} to="/register" className="custom-nav-link">Register</Nav.Link>
+                </>
+              )}
+            </Nav>
+            <div className="ms-auto">
+              <ThemeToggle />
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-    <Outlet />
-  </>
-);
+      <Outlet />
+    </>
+  );
 }
 
 export default Header
