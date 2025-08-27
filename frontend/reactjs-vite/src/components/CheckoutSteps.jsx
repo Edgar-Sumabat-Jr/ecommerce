830a2ed7ef62dc678f32fc2c5efe866cab58cd95
Edgar-Sumabat-Jr <edsumabat2@gmail.com>
2025-08-27 22:51:33 +0800

    added checkoutsteps.jsx

---
+++ frontend/reactjs-vite/src/components/CheckoutSteps.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+function CheckoutSteps({step1, step2, step3, step4,}) {
+  return (
+    <Nav className='justify-content-center mb-4'>
+        <Nav.Item>
+            {step1 ? (
+                <Nav.Link as={Link} to="/login" style={{ color: 'inherit', textDecoration: 'none' }}>Login</Nav.Link>
+            ) : (
+                <Nav.Link disabled>Login</Nav.Link>
+            )}
+        </Nav.Item>
+
+        <Nav.Item>
+            {step2 ? (
+                <Nav.Link as={Link} to="/shipping" style={{ color: 'inherit', textDecoration: 'none' }}>Shipping</Nav.Link>
+            ) : (
+                <Nav.Link disabled>Shipping</Nav.Link>
+            )}
+        </Nav.Item>
+
+        <Nav.Item>
+            {step3 ? (
+                <Nav.Link as={Link} to="/payment" style={{ color: 'inherit', textDecoration: 'none' }}>Payment</Nav.Link>
+            ) : (
+                <Nav.Link disabled>Payment</Nav.Link>
+            )}
+        </Nav.Item>
+
+        <Nav.Item>
+            {step4 ? (
+                <Nav.Link as={Link} to="/place-order" style={{ color: 'inherit', textDecoration: 'none' }}>Place Order</Nav.Link>
+            ) : (
+                <Nav.Link disabled>Place Order</Nav.Link>
+            )}
+        </Nav.Item>
+
+    </Nav>
+  )
+}
+
+export default CheckoutSteps
--- frontend/reactjs-vite/src/pages/ShippingPage.jsx
+++ frontend/reactjs-vite/src/pages/ShippingPage.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
-import FormContainer from '../components/FormContainer'
+
 import { useDispatch, useSelector } from 'react-redux'
 
 // actions
 import { saveShippingAddress } from '../actions/shippingActions'
 import { useNavigate } from 'react-router-dom'
+
+// components
+import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
+
 
 function ShippingPage() {
     const cart = useSelector(state => state.cart)
@@ -28,6 +33,7 @@ function ShippingPage() {
 
     return (
     <FormContainer>
+        <CheckoutSteps step1 step2 />
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler}>
             <Form.Group controlId='address'>
@@ -86,7 +92,7 @@ function ShippingPage() {
         </Form.Group>
 
 
-            <Button type='submit' variant='dark'>
+            <Button type='submit' className="btn btn-dark mt-3">
                 Continue
             </Button>
         </Form>
