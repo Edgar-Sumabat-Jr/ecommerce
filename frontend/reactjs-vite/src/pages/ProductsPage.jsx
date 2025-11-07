f6761cd390e1edd592298b22feb429c02805eeae
Edgar-Sumabat-Jr <edsumabat2@gmail.com>
2025-11-07 17:56:30 +0800

    fixed homepage no product images and fixed Product component

--- frontend/reactjs-vite/src/pages/ProductsPage.jsx
+++ frontend/reactjs-vite/src/pages/ProductsPage.jsx
@@ -46,7 +46,7 @@ function ProductsPage() {
             ) : (
                 <Row>
                     <Col md={6}>
-                        <Image src={product.image} alt={product.name} fluid />
+                        <Image src={`http://127.0.0.1:8000${product.image}`} alt={product.name} fluid />
                     </Col>
 
                     <Col md={3}>
