ede0e3275e5128951c6b6a3c7e56b5c41cdee6a4
Edgar-Sumabat-Jr <edsumabat2@gmail.com>
2026-08-03 23:43:06 +0800

    fixed import error on loader and message components inside profilepage

--- frontend/reactjs-vite/src/pages/ProfilePage.jsx
+++ frontend/reactjs-vite/src/pages/ProfilePage.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col, Table } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import Loader from "../Components/Loader";
-import Message from "../Components/Message";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 import { getUserDetails, updateUserProfile } from "../actions/userActions";
 import { USER_UPDATE_PROFILE_RESET } from "../constants/useConstants";
 
