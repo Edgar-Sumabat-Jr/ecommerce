fd4494f19755da6599b00c73de48a2d26735e850
Edgar-Sumabat-Jr <edsumabat2@gmail.com>
2026-08-06 23:14:21 +0800

    Use Render PORT variable for gunicorn

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,4 +7,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
-exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
