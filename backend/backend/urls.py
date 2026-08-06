2d2dd4862eac0a45d8f96c3d238b7b036466e17b
Edgar-Sumabat-Jr <edsumabat2@gmail.com>
2026-08-06 23:50:54 +0800

    Add media file serving

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -28,6 +28,11 @@ urlpatterns = [
     path("api/health/", health_check, name="health_check"),
 ]
 
-if settings.DEBUG:
-    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+#     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
