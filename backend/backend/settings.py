fce4dc9fbb19d303fd738c381d47954259322eb8
Edgar-Sumabat-Jr <edsumabat2@gmail.com>
2026-08-07 20:30:16 +0800

    fix cloudinary media storage collectstatic

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
 
+    "cloudinary_storage",
     'django.contrib.staticfiles',
     "cloudinary",
-    "cloudinary_storage",
 
     'app1',
     'rest_framework',
