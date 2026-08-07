dfe952180c7539abe3d94de59f7b255f76134564
Edgar-Sumabat-Jr <edsumabat2@gmail.com>
2026-08-07 20:35:41 +0800

    fix cloudinary media storage collectstatic

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -230,7 +230,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
