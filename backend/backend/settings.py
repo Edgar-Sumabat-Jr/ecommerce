bad85a75e553e5b118b767da9bdb774b4ce65b9f
Edgar-Sumabat-Jr <edsumabat2@gmail.com>
2026-08-07 20:23:06 +0800

    Fix staticfiles storage compatibility

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATICFILES_DIRS = [BASE_DIR / "static_my_project", ]
 
